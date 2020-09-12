ef368b38fd01ee6885bfc8eee939ac1d0a6d367c
Damaris Magdaleno <50345706+dmmarr@users.noreply.github.com>
2020-09-11 20:52:05 -0700

    Add files via upload

---
+++ HappyFace.java
@@ -0,0 +1,22 @@
+/*Damaris Magdaleno
+ *Cs A170 
+ *September 8, 2020
+ *Exercise #2 
+  */
+ 
+ public class HappyFace {
+    public static void main (String []args ){
+        System.out.println("  /////");
+        System.out.println(" +\"\"\"\"\"+ ");
+        System.out.println("(| o o |)");
+        System.out.println(" |  ^  | ");
+        System.out.println(" | '-' | ");
+        System.out.println(" +-----+ ");
+        
+    }
+
+    
+    
+    
+    
+    }
